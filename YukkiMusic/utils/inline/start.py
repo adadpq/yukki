b15c49079d3b544284ae4109700bb72084eb7e24
pokiaq <108633399+pokiaq@users.noreply.github.com>
2022-07-07 02:12:31 +0200

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -126,7 +126,8 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     InlineKeyboardButton(
                         text=_["S_B_7"], user_id=OWNER
                     ),
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")]
+                     InlineKeyboardButton(
+                        text=_["ST_B_6"], callback_data="LG"
                ),
             ]
         )
